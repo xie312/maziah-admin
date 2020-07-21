4e0c50b4b9960435d5450d93bf518550b2f47fcb
valor <valor312@gmail.com>
2020-07-21 17:08:40 +0800

    Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import firebase from "firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ChatRoom from "./components/ChatRoom";
+
+// Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCTtEZAQfoTSIffDE_h0B9yrQOOSTe8cIE",
+  authDomain: "notification-603dc.firebaseapp.com",
+  databaseURL: "https://notification-603dc.firebaseio.com",
+  projectId: "notification-603dc",
+  storageBucket: "notification-603dc.appspot.com",
+  messagingSenderId: "865117603471",
+  appId: "1:865117603471:web:e8045c168e01f5c00c52fa",
+  measurementId: "G-DMC6MSJ4TL",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const styles = (theme) => ({
+  root: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+  },
+  appbarTitle: {
+    padding: "20px",
+  },
+});
+
+class App extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Typography variant="h4" align="center" color="textPrimary" className={classes.appbarTitle}>
+            Maziah Admin
+          </Typography>
+        </AppBar>
+
+        <ChatRoom firebase={firebase} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/components/ChatRoom.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+import { TextField, Button, List, ListItem, ListItemText, Paper, Box } from "@material-ui/core";
+import { shadows } from "@material-ui/system";
+import { withStyles } from "@material-ui/core/styles";
+import moment from "moment";
+
+const styles = (theme) => ({
+  messageList: {
+    flexGrow: 1,
+    overflow: "auto",
+    margin: "5px",
+  },
+  form: {
+    margin: "20px",
+    display: "flex",
+  },
+  messageInput: {
+    flexGrow: 1,
+  },
+  submitButton: {
+    marginLeft: "10px",
+  },
+});
+
+class ChatRoom extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: "",
+      messages: [],
+    };
+
+    this.messagesRef = this.props.firebase.database().ref("messages");
+  }
+
+  componentDidMount() {
+    this.messagesRef.limitToLast(20).on("value", (snapshot) => {
+      let newMessages = [];
+      snapshot.forEach((child) => {
+        var message = child.val();
+        newMessages.push({ id: child.key, content: message.content });
+      });
+
+      this.setState({ messages: newMessages });
+
+      this.scrollToBottom();
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({ content: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const content = this.state.content.trim();
+    if (content) {
+      console.log(`:${content}:`);
+
+      this.messagesRef.push({
+        content,
+        sentAt: moment().format("MMMM Do YYYY, h:mm:ss a"),
+      });
+    }
+
+    this.setState({ content: "" });
+    this.scrollToBottom();
+  };
+
+  renderMessages = () => {
+    // debugger;
+
+    return this.state.messages.map((message) => (
+      <ListItem>
+        <Box boxShadow={1} bgcolor="background.paper" p={1}>
+          {message.content}
+        </Box>
+      </ListItem>
+    ));
+  };
+
+  scrollToBottom = () => {
+    this.bottomSpan.scrollIntoView({ behavior: "smooth" });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <>
+        <List className={classes.messageList}>
+          {this.renderMessages()}
+          <span ref={(el) => (this.bottomSpan = el)} />
+        </List>
+
+        <form onSubmit={this.handleSubmit} className={classes.form}>
+          <TextField
+            type="text"
+            value={this.state.content}
+            onChange={this.handleChange}
+            placeholder="Enter message"
+            className={classes.messageInput}
+          />
+          <Button type="submit" variant="contained" color="primary" className={classes.submitButton}>
+            Send
+          </Button>
+        </form>
+      </>
+    );
+  }
+}
+
+export default withStyles(styles)(ChatRoom);
